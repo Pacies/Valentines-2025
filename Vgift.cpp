31cd95c8aa06df8d6276f9532e54bd39c7fd4acc
Pacey <janernest36@gmail.com>
2025-02-09 20:30:57 +0800

    unfinished_lsm

--- Vgift.cpp
+++ Vgift.cpp
@@ -21,18 +21,6 @@ void heartBeat() {
     }
 }
 
-void errorMsg(){
-    cout << "SYSTEM ERROR!!!" << endl;
-    cout << "Error509: Program Malfunction. (Try Rebooting)" << endl;
-    cout << "Rebooting ..." << endl;
-    cout << "..." << endl;
-    cout << "Please Wait Paitently" << endl;
-    cout << "..." << endl;
-    cout << "..." << endl;
-    cout << "..." << endl;
-    cout << "Restarting Program Execution" << endl;
-}
-
 int main() {
     
     cout << "HAPPY VALENTINES REGINE!!! <3" << endl;
