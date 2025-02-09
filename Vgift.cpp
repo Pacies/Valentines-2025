0966065d3fbfd54f5a794a5767c9034f2776e987
Pacey <janernest36@gmail.com>
2025-02-09 20:24:37 +0800

    revision 1

--- Vgift.cpp
+++ Vgift.cpp
@@ -53,7 +53,12 @@ void errQuestion(){
 int main() {
     string firstans; 
 
-
+    cout << "HAPPY VALENTINES REGINE!!! <3" << endl;
+    cout << "Let me first say, I Love You Regine. With all my heart." << endl;
+    cout << "I would walk to the ends of the world so that I could see your smile, the smile the brightens up my day." << endl; 
+    cout << "I would swim across oceans, just to feel the warmth of your embrace." << endl;
+    cout << "Your eyes, that has captivated me from time and time again." << endl;
+    cout << "Your effortless Beauty that knows no end." << endl;
 
     heartBeat();
     return 0;
