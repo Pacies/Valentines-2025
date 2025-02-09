c96992694fbef5a4073363b9d5c3adfcede41660
Pacey <janernest36@gmail.com>
2025-02-09 20:29:58 +0800

    master1

--- Vgift.cpp
+++ Vgift.cpp
@@ -1,34 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void heartBeat() {
-    string heart[] = {
-        "    ______     ______    ",
-        "   |******|   |******|   ",
-        "   |*******| |*******|   ",
-        "    |***************|   ",
-        "     |*************|   ",
-        "   =====================}  ",
-        "       |**********|     ",
-        "        |********|       ",
-        "         |******|        ",
-        "          |****|         ",
-        "           |**|          "
-    };
-
-    for (const string& line : heart) {
-        cout << line << endl;
-    }
-}
-
-void paBebe(){
-    cout << "Hai bebii, ano kamusta ka??" << endl;
-    cout << "ahhh okay lng.., sigii sigii," << endl;
-    cout << "ano kasi, malapit na valentines.." << endl;
-    cout << "gusto sana mag tanong.." << endl;
-    cout << "If you would want to be my Va" << endl;
-}
-
 void errorMsg(){
     cout << "SYSTEM ERROR!!!" << endl;
     cout << "Error509: Program Malfunction. (Try Rebooting)" << endl;
@@ -43,12 +15,7 @@ void errorMsg(){
 
 int main() {
  
-        cout << "HAPPY VALENTINES REGINE!!! <3" << endl;
-    cout << "Let me first say, I Love You Regine. With all my heart." << endl;
-    cout << "I would walk to the ends of the world so that I could see your smile, the smile the brightens up my day." << endl; 
-    cout << "I would swim across oceans, just to feel the warmth of your embrace." << endl;
-    cout << "Your eyes, that has captivated me from time and time again." << endl;
-    cout << "Your effortless Beauty that knows no end." << endl;
+    
     
     return 0;
 }
